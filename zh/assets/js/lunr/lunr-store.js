726ab9d857bb9a5ddb7ac9405db54311bdb4743d
dev <user@users.noreply.github.com>
2021-11-05 04:15:09 +0300

    354

--- zh/assets/js/lunr/lunr-store.js
+++ zh/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": "https://ethminer.tech/assets/images/thumbs/500x300.png"
       },{
         "title": "Download ETHMiner  39.7 - AMD GPU Miner [2021]",
-        "excerpt":" Get started with NBMiner  \t\t \t\t\t \t\tVersion: 0.19.0  \t\tFile: *NBMiner.exe,    \t\tSHA256: 0a9f89cb87a8ba383d970ec3bd314ac339c0e872c4db44c7f7e7ee32403aa3c6  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new ETHMiner stale version is ready.   You can download ETHMiner 39.7 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
+        "excerpt":" Get started with ETHMiner  \t\t \t\t\t \t\tVersion: 0.19.0  \t\tFile: *ETHMiner.exe,    \t\tSHA256: 0a9f89cb87a8ba383d970ec3bd314ac339c0e872c4db44c7f7e7ee32403aa3c6  \t\t(Download for Linux)\t \t\t \t\t \t\t \t\t \t\tDownload Now (Windows x64) \t\t \t\t(mirror) \t\t \t     The new ETHMiner stale version is ready.   You can download ETHMiner 39.7 from here:   Download Now   Download (mirror)   Inside the archive you will find a file README.txt with installation instructions.   Make sure to replace the pool and wallet address by what you’re using in all files.        ","categories": ["download"],
         "tags": ["ETHMiner"],
         "url": "https://ethminer.tech/download/39.7/",
         "teaser": "https://ethminer.tech/assets/images/thumbs/500x300.png"
